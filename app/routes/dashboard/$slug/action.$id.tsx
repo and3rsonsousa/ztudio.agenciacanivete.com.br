b865414af7bd064f00bac381958c35d40d571ab3
Anderson Sousa <and3rsonsousa@outlook.com>
2022-11-10 16:57:07 -0300

    recentes

--- app/routes/dashboard/$slug/action.$id.tsx
+++ app/routes/dashboard/$slug/action.$id.tsx
@@ -1,10 +1,11 @@
 import type { ActionFunction, LoaderFunction } from "@remix-run/cloudflare";
 import { redirect } from "@remix-run/cloudflare";
-import { useActionData, useLoaderData } from "@remix-run/react";
+import { useActionData, useLoaderData, useMatches } from "@remix-run/react";
+import { ActionMedium } from "~/components/Actions";
 import AddActionDialog from "~/components/AddActionDialog";
 import Exclamation from "~/components/Exclamation";
-import { getAction, handleAction } from "~/lib/data";
-import type { ActionModelFull } from "~/lib/models";
+import { getAction, getActions, handleAction } from "~/lib/data";
+import type { ActionModel, ActionModelFull } from "~/lib/models";
 
 export const action: ActionFunction = async ({ request, params }) => {
   const formData = await request.formData();
@@ -16,21 +17,38 @@ export const action: ActionFunction = async ({ request, params }) => {
 };
 
 export const loader: LoaderFunction = async ({ request, params }) => {
-  const { data: action } = await getAction(request, params.id as string);
-  return { action };
+  const [{ data: action }, { data: actions }] = await Promise.all([
+    getAction(request, params.id as string),
+    getActions({ request, account: params.slug }),
+  ]);
+
+  return { action, actions };
 };
 
 export default function ActionPage() {
-  const { action } = useLoaderData<{ action: ActionModelFull }>();
+  const { action, actions } = useLoaderData<{
+    action: ActionModelFull;
+    actions: ActionModel[];
+  }>();
   const actionData = useActionData<{ error: { message: string } }>();
 
   return (
-    <div className="h-full overflow-hidden overflow-y-auto  p-4 lg:p-8">
-      <div className="mx-auto max-w-2xl">
-        {actionData && (
-          <Exclamation type="error">{actionData.error.message}</Exclamation>
-        )}
-        <AddActionDialog action={action} />
+    <div className="p-4 lg:p-8">
+      <div className="mx-auto h-full max-w-4xl grid-cols-3 overflow-hidden lg:grid">
+        <div className="col-span-2 h-full px-4">
+          {actionData && (
+            <Exclamation type="error">{actionData.error.message}</Exclamation>
+          )}
+          <AddActionDialog action={action} />
+        </div>
+        <div className="col-span-1 h-full px-4">
+          <h4>Ações recentes</h4>
+          <div className="no-scrollbars overflow-hidden overflow-y-auto">
+            {actions.map((action) => (
+              <ActionMedium action={action} key={action.id} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
