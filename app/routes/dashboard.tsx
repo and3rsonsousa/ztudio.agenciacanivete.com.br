846b0f6b1b1149861eb39bb47a7368fb7e1d9776
Anderson Sousa <and3rsonsousa@outlook.com>
2022-12-06 17:22:24 -0300

    no loader

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -11,34 +11,34 @@ import {
   getTagsStatus,
 } from "~/lib/data";
 
-export const loader: LoaderFunction = async ({ request }) => {
-  const {
-    data: { session },
-  } = await getUser(request);
+// export const loader: LoaderFunction = async ({ request }) => {
+//   const {
+//     data: { session },
+//   } = await getUser(request);
 
-  if (!session) {
-    return redirect("/login");
-  } else {
-    const userId = session.user.id;
+//   if (!session) {
+//     return redirect("/login");
+//   } else {
+//     const userId = session.user.id;
 
-    const [
-      { data: person },
-      { data: persons },
-      { data: accounts },
-      { tags, status },
-      { data: celebrations },
-    ] = await Promise.all([
-      getPersonByUser(userId, request),
-      getPersons(request),
-      getAccounts(userId, request),
-      getTagsStatus(request),
-      getCelebrations({ request }),
-    ]);
+//     const [
+//       { data: person },
+//       { data: persons },
+//       { data: accounts },
+//       { tags, status },
+//       { data: celebrations },
+//     ] = await Promise.all([
+//       getPersonByUser(userId, request),
+//       getPersons(request),
+//       getAccounts(userId, request),
+//       getTagsStatus(request),
+//       getCelebrations({ request }),
+//     ]);
 
-    const url = request.url;
-    return { person, persons, accounts, tags, status, celebrations, url };
-  }
-};
+//     const url = request.url;
+//     return { person, persons, accounts, tags, status, celebrations, url };
+//   }
+// };
 
 export default function Dashboard() {
   return <div>Teste</div>;
