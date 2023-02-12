e2fe646f692ac30e3ca65270937eba83c6f9089a
Anderson Sousa <and3rsonsousa@outlook.com>
2023-02-12 19:58:17 -0300

    no pixel

--- app/root.tsx
+++ app/root.tsx
@@ -132,34 +132,7 @@ export function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Partytown
-          forward={["fbq"]}
-          debug={true}
-          resolveUrl={function (url) {
-            if (url.hostname === "connect.facebook.net") {
-              var proxyUrl = new URL("https://ztudio.agenciacanivete.com.br/");
-              proxyUrl.searchParams.append("url", url.href);
-              return proxyUrl;
-            }
-            return url;
-          }}
-        />
-        <script
-          type="text/partytown"
-          async
-          dangerouslySetInnerHTML={{
-            __html: `setTimeout(function(){!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1017323528864709');
-fbq('track', 'PageView');}, 1000)`,
-          }}
-        ></script>
+
         <Meta />
         <Links />
       </head>
@@ -168,16 +141,6 @@ fbq('track', 'PageView');}, 1000)`,
           <Outlet context={context} />
         </div>
         <ScrollRestoration />
-
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1017323528864709&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
 
         <Scripts />
         <LiveReload />
