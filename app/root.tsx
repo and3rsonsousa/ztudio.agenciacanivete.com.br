61e3966c93465858c01e4fee2ec662feba5704db
Anderson Sousa <and3rsonsousa@outlook.com>
2023-02-12 01:35:53 -0300

    timeout

--- app/root.tsx
+++ app/root.tsx
@@ -132,12 +132,10 @@ export function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Partytown debug={true} forward={["fbq"]} />
+
         <script
-          async
-          type="text/partytown"
           dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s)
+            __html: `setTimeout(function(){!function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -146,7 +144,7 @@ t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '1017323528864709');
-fbq('track', 'PageView');`,
+fbq('track', 'PageView');}, 1000)`,
           }}
         ></script>
         <Meta />
